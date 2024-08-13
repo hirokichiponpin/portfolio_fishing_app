8f198e15a34d307c1dc80a00b0812fa83b2dfbf8
hirokichiponpin <hiroki.s_3.26@icloud.com>
2024-08-14 03:00:02 +0900

    ユーザ情報に説明文を追加、ユーザ情報編集時にパスワード入力を行わないよう変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,5 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     user_path(resource) 
   end
+
+  def update_resource(resource, params)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+    resource.update_without_password(params)
+  end
 end
   
